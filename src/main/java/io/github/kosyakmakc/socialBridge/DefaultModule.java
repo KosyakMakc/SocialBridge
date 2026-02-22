9e797b0abb0f2154635eb99a821d488fa615bd67
Chovnyuk <kosyakmakc@live.com>
2026-02-22 14:31:24 +0500

    clean up

--- src/main/java/io/github/kosyakmakc/socialBridge/DefaultModule.java
+++ src/main/java/io/github/kosyakmakc/socialBridge/DefaultModule.java
@@ -3,12 +3,9 @@ package io.github.kosyakmakc.socialBridge;
 import io.github.kosyakmakc.socialBridge.DatabasePlatform.DefaultTranslations.English;
 import io.github.kosyakmakc.socialBridge.DatabasePlatform.DefaultTranslations.Russian;
 import io.github.kosyakmakc.socialBridge.MinecraftPlatform.IMinecraftPlatform;
-import io.github.kosyakmakc.socialBridge.MinecraftPlatform.MinecraftUser;
 import io.github.kosyakmakc.socialBridge.Modules.SocialModule;
+
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
-import java.util.logging.Logger;
 
 public class DefaultModule extends SocialModule {
     public static final UUID MODULE_ID = UUID.fromString("dcab3770-b24e-44bb-b9a9-19edf96b9986");
@@ -25,27 +22,5 @@ public class DefaultModule extends SocialModule {
 
         addTranslationSource(new English());
         addTranslationSource(new Russian());
-    }
-
-    @Override
-    public CompletableFuture<Boolean> enable(ISocialBridge bridge) {
-        var result = super.enable(bridge);
-        bridge.getMinecraftPlatform().getPlayerJoinEvent().addHandler(new Consumer<MinecraftUser>() {
-
-            @Override
-            public void accept(MinecraftUser t) {
-                Logger.getGlobal().info("CONNECTED " + t.getName());
-            }
-            
-        });
-        bridge.getMinecraftPlatform().getPlayerLeaveEvent().addHandler(new Consumer<MinecraftUser>() {
-
-            @Override
-            public void accept(MinecraftUser t) {
-                Logger.getGlobal().info("LEAVE " + t.getName());
-            }
-            
-        });
-        return result;
     }
 }
